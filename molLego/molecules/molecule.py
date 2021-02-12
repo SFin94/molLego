3434acab09efcbc40a9aef26008bcb54ce7cfc9f
Sophie <smf115@ic.ac.uk>
2021-02-12 19:07:28 +0000

    Updated set parameter and current get methods to work in new class format

--- molLego/molecules/molecule.py
+++ molLego/molecules/molecule.py
@@ -1,5 +1,6 @@
 """Module containing Molecule definiton."""
-
+import sys
+import numpy as np
 import molLego.utilities.geom as geom
 
 __ATOM_ELEMENTS__ = ['h',  'he', 'li', 'be', 'b',  'c',  'n',  'o',  'f',  'ne', 'na', 'mg', 'al', 'si', 'p',  's',  'cl', 'ar', 'k',  'ca', 'sc', 'ti', 'v ', 'cr', 'mn', 'fe', 'co', 'ni', 'cu', 'zn', 'ga', 'ge', 'as', 'se', 'br', 'kr', 'rb', 'sr', 'y',  'zr', 'nb', 'mo', 'tc', 'ru', 'rh', 'pd', 'ag', 'cd', 'in', 'sn', 'sb', 'te', 'i',  'xe', 'cs', 'ba', 'la', 'ce', 'pr', 'nd', 'pm', 'sm', 'eu', 'gd', 'tb', 'dy', 'ho', 'er', 'tm', 'yb', 'lu', 'hf', 'ta', 'w',  're', 'os', 'ir', 'pt', 'au', 'hg', 'tl', 'pb', 'bi', 'po', 'at', 'rn', 'fr', 'ra', 'ac', 'th', 'pa', 'u', 'np', 'pu']
@@ -24,8 +25,13 @@ class Molecule():
         A ``(N, 3)`` array of x, y, z coorindates for each atom.
         Where N is the number of atoms in the molecule.
     
-    e: :class:`float`
+    e : :class:`float`
         The energy of the molecule.
+
+    parameters : :class:`dict`
+        Where key is the parameter identifier,
+        and value is the geometric parameter.
+        E.g. O1-C2-O3: 180.00
         
     """
 
@@ -55,31 +61,129 @@ class Molecule():
         self.charge = properties['charge']
         self.geometry = properties['geom']
         self.e = properties['energy']
+        self.parameters = {}
 
-    
-    # Add representation strings?
-
-
-    def set_parameters(self, param):
+    def set_parameters(self, params):
         """
-        Calculate geometric parameters and set as property.
-        WHETHER TO HAVE AS MULTIPLE OR NOT?
+        Calculate geometric parameters (bonds, angles, dihedrals).
+
+        Parameters saved in :class: `dict`.
+        Where key is `str` of each atom_id and atom_index.
+        E.g. C1-H2
+        Value is the value of the geometric parameter.
+
+        Parameters
+        ----------
+        params : :class:`iterable` of :class:`int`
+            The atom indexes defining the geometric parameter.
+            Can be either a nested iterable is multiple parameters,
+            or a single iterable if only one parameter is required.
+
+        """
+        # Initialise values.
+        param_keys = []
+        param_vals = []
+
+        # Handle if single parameter.
+        if not any(isinstance(x, (list, tuple)) for x in params):
+            params = [params]
+
+        for par in params:
+            # Set parameter key as atom_ID+atom_index'-'atom_ID+atom_index [-...-...]
+            param_keys.append('-'.join([self.atom_ids[i] + str(i) for i in par]))
+            param_vals = geom.calc_param(par, self.geometry)
+
+        # Update parameter dict.
+        self.parameters.update(dict(zip(param_keys, param_vals)))
+
+    def get_atom_names(self, atom_index=None):
+        """
+        Yield unique name (atom_id+atom_index) of atom in molecule.
+
+        Example would be `C1` for the C at index 1 in molecule.
+
+        Parameters
+        ----------
+        atom_index: :class: `iterable` of :class:`int`
+            The index(es) of the atoms to return names for.
+            [default: ``None``] If ``None`` then returns for all
+            atom in the molecule.
+            Can be single `int` to call name for single atom.
+
+        Yields
+        -------
+        :class: `int`
+            Unique name of an atom made of atom_id+atom_index.
+
+        """
+        # Set to all atoms is atom_index is None.
+        if atom_index is None:
+            atom_index = range(self.atom_number)
+        elif isinstance(atom_index, int):
+            atom_index = (atom_index, )
+
+        atomic_names = [self.atom_ids[i] + str(i) for i in atom_index]
+
+        for atom_name in atomic_names:
+            yield atom_name
+        
+    def get_atomic_numbers(self, atom_index=None):
+        """
+        Yield atomic number of atom.
         
         Parameters
         ----------
-        params : :class:`nested list of `int``
-            List of the atom indexes of the geometric parameters.
+        atom_index: :class: `iterable` of :class:`int`
+            The index(es) of the atoms to return names for.
+            [default: ``None``] If ``None`` then returns for all
+            atom in the molecule.
+            Can be single `int` to call name for single atom.
+
+        Yields
+        -------
+        :class: `int`
+            The atomic number of an atom.
 
         """
-        # Set parameter key as atom_ID+atom_index'-'atom_ID+atom_index [-...-...]
-        param_key = '-'.join([atom_ids[i] + str(i) for i in param])
-        param_val = geom.calc_param(param, self.geom)
-        # Up to - to change
-        if 'parameters' in self.__dict__:
-            self.parameters.update(dict(zip(param_keys, param_values)))
-        else:
-            self.parameters = dict(zip(param_keys, param_values))
+        # Set to all atoms is atom_index is None.
+        if atom_index is None:
+            atom_index = range(self.atom_number)
+        elif isinstance(atom_index, int):
+            atom_index = (atom_index, )
 
+        atomic_numbers = [int(__ATOM_ELEMENTS__.index(
+                          self.atom_ids[i].lower())) + 1
+                          for i in atom_index]
+
+        for atom_number in atomic_numbers:
+            yield atom_number
+
+    def reindex_molecule(self, reindex):
+        """
+        Reorder molecule geometry and atom list from a given mapping.
+
+        Example
+        -------
+        Mapping is a list of the new positions for each atom.
+        Indexes start at 0.
+
+        For a molecule with 4 atoms with start order: [H C H O],
+        
+        To switch the position of the O and C atoms.
+        Input reindex map: [0 3 2 1]
+        Will keep H0 at 0; move C to position 3; 
+        keep H2 at 2 and move O3 to 1.
+        
+        Returned order: [H O H C]
+
+        Parameters
+        ----------
+        reindex: `list of int`
+            List of new index positions for each atom.
+    
+        """
+        self.geom = self.geom[reindex, :]
+        self.atom_ids = [self.atom_ids[i] for i in reindex]
 
     def set_adjacency(self, distance=2.0):
 
@@ -97,34 +201,6 @@ class Molecule():
             for j, atom_j in enumerate(self.geom[i+1:]):
                 self.adjacency[i, j+i+1] =  (geom.calc_dist(atom_i, atom_j) < distance)
         self.adjacency += self.adjacency.transpose()
-
-
-    def set_atom_indexes(self):
-
-        """Class method to convert list of atom ids to list of corresponding atom indexes
-
-        Sets class attributes:
-         atom_indexes: :class:`list` - atom ids as str entry
-
-        """
-        
-        self.atom_indexes = [int(__ATOM_ELEMENTS__.index(i.lower()))+1 for i in self.atom_ids]
-
-
-    def reindex_molecule(self, reindex):
-
-        """Class method to reorder a molecules geometry and atom list based on a given mapping
-        Updates class attributes:
-         atoms: :class:`list of str` - atom IDs of the atoms in the molecule
-         geom: :class:`ndarray` (dim: :class:atom_number, 3; float) - Results array of x, y, z coordinates for each atom in the molecule
-
-        Parameters:
-        reindex: list of int - list of new index positions
-        """
-
-        self.geom = self.geom[reindex, :]
-        self.atom_ids = [self.atom_ids[i] for i in reindex]
-
 
 
 
