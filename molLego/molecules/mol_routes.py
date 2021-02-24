e687e40a793a569b9f6d5b18b1e315b580062c0a
Sophie <smf115@ic.ac.uk>
2021-02-24 12:33:27 +0000

    added chain to handle iterable in mols_to_dataframe

--- molLego/molecules/mol_routes.py
+++ molLego/molecules/mol_routes.py
@@ -2,6 +2,7 @@
 import sys
 import numpy as np
 import pandas as pd
+import itertools
 
 import molLego.parsers.parse_gaussian as pgauss
 import molLego.utilities.geom as geom
@@ -94,16 +95,14 @@ def mols_to_dataframe(mols, mol_names=None, save=None, mol_zero=None):
         DataFrame of Molecules and properties.
      
     """
-    mol_data = []
-    for i, mol in enumerate(mols):
-        mol_data.append(mol.get_df_repr())
-
-    # Set index from file name if names not given.
-    if mol_names == None:
-        mol_names = [mol.parser.file_name.split('/')[-1].split('.')[0]
-                     for mol in mols]
+    # Create data frame representations.
+    mol_data = [mol.get_df_repr() for mol in mols]
     
-    # Create data frame and calculate relative values.
+    # Handle possible nested dicts.
+    if not isinstance(mol_data[0], dict):
+        mol_data = itertools.chain(*mol_data)
+    
+    # Create dataframe and calculate relative values.
     molecule_df = pd.DataFrame(mol_data, index=mol_names)
     if isinstance(mol_zero, int):
         mol_zero = mol_names[mol_zero]
@@ -186,53 +185,6 @@ def parse_tracked_params(system_file):
             tracked_params[param[0]] = indexes
     return tracked_params
 
-
-def init_scan(*args, tracked_params=None):
-
-    """Function that generates a list of molecule objects from a scan file
-
-    Parameters:
-     args: str - gaussian log files of scan results
-     tracked_params: [optional] str - file with tracked parameters in (gaussian indexes)
-
-    Returns:
-     scan_molecules: List of Molecule objects for each step of scan
-    """
-
-    # Inititalise variables
-    scan_molecules = []
-
-    # Parse in tracked parameters if set
-    if tracked_params != None:
-        parameters = parse_tracked_params(tracked_params)
-    else:
-        parameters = {}
-
-    for i, input_file in enumerate(args):
-
-        # Initialise gaussian log file object
-        scan_file = pgauss.GaussianLog(input_file)
-
-        # Get scan info - depending if rigid or relaxed scan
-        if scan_file.job_type == 'scan_relaxed':
-            scan_info = scan_file.get_scan_info()
-            # Set scan parameter in parameters dict and range of opt steps in file
-            parameters[scan_info['param_key']] = scan_info['atom_inds']
-            opt_steps = list(range(1, scan_info['num_steps']+2))
-        else:
-            # If rigid scan then needs more processing as potentially has mutltiple scan parameters
-            scan_info = scan_file.get_rigid_scan_info()
-            total_scan_steps = 1
-            # Set scan parameters in parameters dict and range of opt steps in file
-            for scan_parameter in list(scan_info.values()):
-                parameters[scan_parameter['param_key']] = scan_parameter['atom_inds']
-                total_scan_steps *= (scan_parameter['num_steps'] + 1)
-            opt_steps = list(range(1, total_scan_steps+1))
-
-        # scan_molecules += molecules.init_mol_from_log(input_file, opt_steps=opt_steps, parameters=parameters)
-
-    return scan_molecules, scan_info
-
 def sum_mols(*args):
 
     """
@@ -291,41 +243,3 @@ def sum_mols(*args):
 
     return new_mol
 
-def process_input_file(input_file):
-    """
-    Process an input file to Molecule Objects and/or a DataFrame.
-    
-    If a .conf file is given the Molecule objects are processed for each entry in the .conf file.
-    If a .csv file then the information is parsed directly into a moleucle DataFrame.
-    
-    Parameters
-    ----------
-    input_file: `str`
-        File name with file type of: .conf or .csv
-
-    Returns
-    -------
-    mol_df: pandas DataFrame`
-        Molecule information and properties.
-    molecules: `list of Molecule object`
-        Molecule objects for each entry line in .conf file.
-        [Only returned if input file type is conf.
-    
-    """
-    # Retrieve file type for input file.
-    file_type = str(input_file.split('.')[-1])
-
-    # Process conf file, creating Molecule objects and a DataFrame.
-    if file_type == 'conf':
-        mol_names, molecules = construct_mols(input_file)
-        mol_df = mols_to_dataframe(molecules, mol_names=mol_names)
-        return mol_df, molecules
-
-    # Parse in existing dataframe and set first column (mol_names) as index.
-    elif file_type == 'csv':
-        mol_df = pd.read_csv(input_file, index_col=0)
-        return mol_df
-
-    # Raise exception if file type is not recognised.
-    else:
-        raise Exception('File extension not recognised (should be .conf or .csv)')
