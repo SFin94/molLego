cf1e644961d2403ac1c077c8efb7874b620da225
Sophie <smf115@ic.ac.uk>
2021-02-24 12:35:57 +0000

    Minor changes

--- molLego/parsers/parser.py
+++ molLego/parsers/parser.py
@@ -1,30 +1,10 @@
-"""Module containing parent parser class."""
+"""Module containing parent Parser class for output files."""
 class OutputParser():
     """
     Parser base class for calculation output file.
 
-    Attributes
-    ----------
-    atom_ids : :class:`list of str`
-        The IDs of the atoms in the molecule.
-
-    atom_number : :class:`int`
-        The number of atoms in the molecule.
- 
-    charge : :class:`int`
-        The charge of the molecule.
-
-    elements : :class:`list of str`
-        The list of elements present in the molecule.
-
-    file_name : :class:`str`
-        The path to the parent log file.
-
-    method : :class:`str`
-        The method of the calculation (functional, etc.).
-
-    normal_termination : :class:`Bool`
-        ``True`` if normal termination. Otherwise, ``False``.
+    Has to set atom_ids, charge, 
+    geometry and energy for molecule.
 
     """
     
@@ -39,12 +19,16 @@ class OutputParser():
         """Pull the atom IDs from calculation output file."""
         raise NotImplementedError
 
-    def _pull_geometry(self):
-        """Pull the geometry from calculation output file."""
+    def _pull_charge(self):
+        """Pull the energy from calculation output file."""
         raise NotImplementedError
 
     def _pull_energy(self):
         """Pull the energy from calculation output file."""
+        raise NotImplementedError
+
+    def _pull_geometry(self):
+        """Pull the geometry from calculation output file."""
         raise NotImplementedError
 
     def get_properties(self):
